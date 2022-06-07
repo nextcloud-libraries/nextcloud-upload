a434d36cb5e7078ece905cd119f90571112ae020
John Molakvoæ <skjnldsv@protonmail.com>
2022-06-07 13:24:38 +0200

    Improve UploadData testing
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

---
+++ __mocks__/@nextcloud/axios.ts
@@ -0,0 +1,33 @@
+// Fake cancellable timeout
+// TODO: replace by import { setTimeout } from 'timers/promises' when min LTS is >= 16
+// implemented in node 15
+const timeout = (timeout: number, data = {}, signal: AbortSignal = new AbortController().signal) => {
+	return new Promise((resolve, reject) => {
+		if (signal?.aborted){
+			reject(new DOMException("Aborted", "AbortError"))
+			return
+		}
+		signal.addEventListener('abort', () => {
+			reject(new DOMException("Aborted", "AbortError"))
+		})
+
+		setTimeout(() => {
+			if (!signal?.aborted) {
+				resolve(data)
+			}
+		}, timeout)
+	})
+}
+
+export default {
+	async request(data) {
+		// Fake upload progress
+		if (data.onUploadProgress) {
+			await timeout(200)
+			data.onUploadProgress()
+		}
+
+		// Simulate a 500ms request
+		return await timeout(500, data, data.signal)
+	},
+}
