4d44fb35386b54b92de04995c2ed09263175843b
John Molakvoæ <skjnldsv@protonmail.com>
2023-08-29 17:15:33 +0200

    chore: migrate to vite & vitest
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- __tests__/utils/upload.spec.ts
+++ __tests__/utils/upload.spec.ts
@@ -1,23 +1,23 @@
-import { getChunk, initChunkWorkspace, uploadData } from '../../lib/utils/upload.js'
-import axios from '@nextcloud/axios';
-import { jest } from '@jest/globals'
+import { beforeAll, afterAll, describe, expect, test, vi } from 'vitest'
+import axios from '@nextcloud/axios'
 
-const axiosMock: jest.Mock<typeof axios> | typeof axios = axios
+import { getChunk, initChunkWorkspace, uploadData } from '../../lib/utils/upload.js'
+
+const axiosMock: vi.Mock<typeof axios> | typeof axios = axios
 
 beforeAll(() => {
-	jest.mock("axios", jest.fn())
+	vi.mock('axios', vi.fn())
 })
 afterAll(() => {
-	jest.unmock("axios")
+	vi.unmock('axios')
 })
-
 
 describe('Get chunk from file', () => {
 	test('Chunking a valid file', async () => {
 		const blob = new Blob([new ArrayBuffer(5 * 1024 * 1024)])
 		const file = new File([blob as BlobPart], 'image.jpg', {
 			type: 'image/jpeg',
-			lastModified:new Date().getTime()
+			lastModified: new Date().getTime(),
 		})
 
 		const chunk = await getChunk(file, 0, 10 * 1024 * 1024)
@@ -28,7 +28,7 @@ describe('Get chunk from file', () => {
 		const blob = new Blob([new ArrayBuffer(50 * 1024 * 1024)])
 		const file = new File([blob as BlobPart], 'image.jpg', {
 			type: 'image/jpeg',
-			lastModified:new Date().getTime()
+			lastModified: new Date().getTime(),
 		})
 
 		const chunk = await getChunk(file, 0, 10 * 1024 * 1024)
@@ -45,19 +45,19 @@ describe('Get chunk from file', () => {
 
 describe('Initialize chunks upload temporary workspace', () => {
 	test('Init random workspace', async () => {
-		axiosMock.request = jest.fn((config: any) => Promise.resolve(config?.onUploadProgress?.()))
+		axiosMock.request = vi.fn((config: any) => Promise.resolve(config?.onUploadProgress?.()))
 
 		// mock the current location for our assert on the URL
-		Object.defineProperty(window, "location", {
-			value: new URL("https://cloud.domain.com"),
+		Object.defineProperty(window, 'location', {
+			value: new URL('https://cloud.domain.com'),
 			configurable: true,
-		});
+		})
 
 		// mock the current user
 		document.head.setAttribute('data-user', 'test')
 
 		const url = await initChunkWorkspace()
-		
+
 		expect(url.startsWith('https://cloud.domain.com/remote.php/dav/uploads/test/web-file-upload-')).toBe(true)
 		expect(url.length).toEqual('https://cloud.domain.com/remote.php/dav/uploads/test/web-file-upload-3ec6b932e672fd7c1d8430b0f8457b07'.length)
 
@@ -71,12 +71,12 @@ describe('Initialize chunks upload temporary workspace', () => {
 
 describe('Upload data', () => {
 	test('Upload data stream', async () => {
-		axiosMock.request = jest.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
+		axiosMock.request = vi.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
 
 		const url = 'https://cloud.domain.com/remote.php/dav/files/test/image.jpg'
 		const blob = new Blob([new ArrayBuffer(50 * 1024 * 1024)])
-		const signal =  new AbortController().signal
-		const onUploadProgress = jest.fn()
+		const signal = new AbortController().signal
+		const onUploadProgress = vi.fn()
 		await uploadData(url, blob, signal, onUploadProgress)
 
 		expect(onUploadProgress).toHaveBeenCalledTimes(1)
@@ -90,13 +90,13 @@ describe('Upload data', () => {
 		})
 	})
 	test('Upload async data stream', async () => {
-		axiosMock.request = jest.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
+		axiosMock.request = vi.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
 
 		const url = 'https://cloud.domain.com/remote.php/dav/files/test/image.jpg'
 		const blob = new Blob([new ArrayBuffer(50 * 1024 * 1024)])
-		const data = jest.fn(async () => blob)
-		const signal =  new AbortController().signal
-		const onUploadProgress = jest.fn()
+		const data = vi.fn(async () => blob)
+		const signal = new AbortController().signal
+		const onUploadProgress = vi.fn()
 
 		await uploadData(url, data, signal, onUploadProgress)
 
@@ -113,14 +113,14 @@ describe('Upload data', () => {
 	})
 
 	test('Upload cancellation', async () => {
-		axiosMock.request = jest.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
+		axiosMock.request = vi.fn((config: any) => Promise.resolve(config?.onUploadProgress()))
 
 		const url = 'https://cloud.domain.com/remote.php/dav/files/test/image.jpg'
 		const blob = new Blob([new ArrayBuffer(50 * 1024 * 1024)])
-		const data = jest.fn(async () => blob)
-		const controller =  new AbortController()
-		const onUploadProgress = jest.fn()
-		jest.spyOn(controller, 'abort')
+		const data = vi.fn(async () => blob)
+		const controller = new AbortController()
+		const onUploadProgress = vi.fn()
+		vi.spyOn(controller, 'abort')
 
 		// Cancel after 200ms
 		setTimeout(() => controller.abort(), 400)
--- lib/utils/l10n.ts
+++ lib/utils/l10n.ts
@@ -1,17 +1,34 @@
+/**
+ * @copyright Copyright (c) 2023 Ferdinand Thiessen <opensource@fthiessen.de>
+ *
+ * @author Ferdinand Thiessen <opensource@fthiessen.de>
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 import { getGettextBuilder } from '@nextcloud/l10n/gettext'
-
-type Translation = {
-	locale: string;
-	json: object;
-}
 
 const gtBuilder = getGettextBuilder()
 	.detectLocale()
 
-const translations = (process.env.TRANSLATIONS as unknown as Array<Translation>)
-translations.forEach(data => gtBuilder.addTranslation(data.locale, data.json))
+// @ts-expect-error __TRANSLATIONS__ is replaced by vite
+__TRANSLATIONS__.map(data => gtBuilder.addTranslation(data.locale, data.json))
 
 const gt = gtBuilder.build()
 
-export const n = gt.ngettext.bind(gt)
-export const t = gt.gettext.bind(gt)
+export const n = gt.ngettext.bind(gt) as typeof gt.ngettext
+export const t = gt.gettext.bind(gt) as typeof gt.gettext
