284a06aba19e810187bd3ca7b450c620a72d93a0
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-26 18:52:51 +0200

    feat(uploader): Allow to upload directories / allow bulk upload
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/uploader.ts
+++ lib/uploader.ts
@@ -1,10 +1,12 @@
 import type { AxiosError, AxiosResponse } from 'axios'
+import type { WebDAVClient } from 'webdav'
 
 import { CanceledError } from 'axios'
 import { encodePath } from '@nextcloud/paths'
-import { Folder, Permission } from '@nextcloud/files'
+import { Folder, Permission, davGetClient } from '@nextcloud/files'
 import { generateRemoteUrl } from '@nextcloud/router'
 import { getCurrentUser } from '@nextcloud/auth'
+import { normalize } from 'path'
 import axios from '@nextcloud/axios'
 import PCancelable from 'p-cancelable'
 import PQueue from 'p-queue'
@@ -12,6 +14,9 @@ import PQueue from 'p-queue'
 import { getChunk, initChunkWorkspace, uploadData } from './utils/upload.js'
 import { getMaxChunksSize } from './utils/config.js'
 import { Status as UploadStatus, Upload } from './upload.js'
+import { isFileSystemFileEntry } from './utils/filesystem.js'
+import { Directory } from './utils/fileTree.js'
+import { t } from './utils/l10n.js'
 import logger from './utils/logger.js'
 
 export enum Status {
@@ -62,6 +67,9 @@ export class Uploader {
 			})
 		}
 		this.destination = destinationFolder
+
+		// Reset when upload queue is done
+		this._jobQueue.addListener('idle', () => this.reset())
 
 		logger.debug('Upload workspace initialized', {
 			destination: this.destination,
@@ -164,33 +172,198 @@ export class Uploader {
 	}
 
 	/**
+	 * Notify listeners of the upload completion
+	 * @param upload The upload that finished
+	 */
+	private _notifyAll(upload: Upload): void {
+		for (const notifier of this._notifiers) {
+			try {
+				notifier(upload)
+			} catch (error) {
+				logger.warn('Error in upload notifier', { error, source: upload.source })
+			}
+		}
+	}
+
+	/**
+	 * Uploads multiple files or folders while preserving the relative path (if available)
+	 * @param {string} destination The destination path relative to the root folder. e.g. /foo/bar (a file "a.txt" will be uploaded then to "/foo/bar/a.txt")
+	 * @param {Array<File|FileSystemEntry>} files The files and/or folders to upload
+	 * @param {Function} callback Callback that receives the nodes in the current folder and the current path to allow resolving conflicts, all nodes that are returned will be uploaded (if a folder does not exist it will be created)
+	 * @return Cancelable promise that resolves to an array of uploads
+	 *
+	 * @example
+	 * ```ts
+	 * // For example this is from handling the onchange event of an input[type=file]
+	 * async handleFiles(files: File[]) {
+	 *   this.uploads = await this.uploader.batchUpload('uploads', files, this.handleConflicts)
+	 * }
+	 *
+	 * async handleConflicts(nodes: File[], currentPath: string) {
+	 *   const conflicts = getConflicts(nodes, this.fetchContent(currentPath))
+	 *   if (conficts.length === 0) {
+	 *     // No conflicts so upload all
+	 *     return nodes
+	 *   } else {
+	 *     // Open the conflict picker to resolve conflicts
+	 *     try {
+	 *       const { selected, renamed } = await openConflictPicker(currentPath, conflicts, this.fetchContent(currentPath), { recursive: true })
+	 *       return [...selected, ...renamed]
+	 *     } catch (e) {
+	 *       return false
+	 *     }
+	 *   }
+	 * }
+	 * ```
+	 */
+	batchUpload(
+		destination: string,
+		files: (File|FileSystemEntry)[],
+		callback?: (nodes: Array<File|Directory>,
+		currentPath: string) => Promise<Array<File|Directory>|false>,
+	): PCancelable<Upload[]> {
+		const rootFolder = new Directory('', files)
+		if (!callback) {
+			callback = async (files: Array<File|Directory>) => files
+		}
+
+		try {
+			// Increase concurrency to 4 to keep 3 parallel uploads as one if blocked by the directory meta-upload
+			this._jobQueue.concurrency += 1
+
+			return new PCancelable(async (resolve, reject, onCancel) => {
+				try {
+					const value = await this._jobQueue.add(() => {
+						const promise = this.uploadDirectory(destination, rootFolder, callback, davGetClient(this.root))
+						onCancel(() => promise.cancel())
+						return promise
+					})
+					if (value) {
+						resolve(value)
+					}
+				} catch (error) {
+					logger.error('Error in batch upload', { error })
+				}
+				reject(t('Upload has been cancelled'))
+			})
+		} finally {
+			// Reset concurrency
+			this._jobQueue.concurrency -= 1
+		}
+	}
+
+	// Helper for uploading directories (recursivly)
+	private uploadDirectory(
+		destination: string,
+		directory: Directory,
+		callback: (nodes: Array<File|Directory>, currentPath: string) => Promise<Array<File|Directory>|false>,
+		// client as parameter to cache it for performance
+		client: WebDAVClient,
+	): PCancelable<Upload[]> {
+		const folderPath = normalize(`${destination}/${directory.name}`).replace(/\/$/, '')
+		const rootPath = `${this.root.replace(/\/$/, '')}/${folderPath.replace(/^\//, '')}`
+
+		return new PCancelable(async (resolve, reject, onCancel) => {
+			const abort = new AbortController()
+			onCancel(() => abort.abort())
+
+			// Let the user handle conflicts
+			const selectedForUpload = await callback(directory.children, folderPath)
+			if (selectedForUpload === false) {
+				reject(t('Upload has been cancelled'))
+				return
+			}
+
+			const directories: PCancelable<Upload[]>[] = []
+			const uploads: PCancelable<Upload>[] = []
+			const currentUpload: Upload = new Upload(rootPath, false, 0, directory)
+			currentUpload.signal.addEventListener('abort', () => reject(t('Upload has been cancelled')))
+			currentUpload.status = UploadStatus.UPLOADING
+
+			try {
+				// Wait for own directory to be created (if not the virtual root)
+				if (directory.name) {
+					try {
+						await client.createDirectory(folderPath, { signal: abort.signal })
+						// We add the "upload" to get some information of changed nodes
+						uploads.push(new PCancelable((resolve) => resolve(currentUpload!)))
+						this._uploadQueue.push(currentUpload)
+					} catch (error) {
+						if (error && typeof error === 'object' && 'status' in error && error.status === 405) {
+							// Directory already exists, so just write into it and ignore the error
+							logger.debug('Directory already exists, writing into it', { directory: directory.name })
+						} else {
+							// Another error happend, so abort uploading the directory
+							throw error
+						}
+					}
+				}
+
+				for (const node of selectedForUpload) {
+					if (node instanceof Directory) {
+						directories.push(this.uploadDirectory(folderPath, node, callback, client))
+					} else {
+						uploads.push(this.upload(`${folderPath}/${node.name}`, node))
+					}
+				}
+
+				abort.signal.addEventListener('abort', () => {
+					uploads.forEach((upload) => upload.cancel())
+					directories.forEach((upload) => upload.cancel())
+				})
+
+				const resolvedUploads = await Promise.all(uploads)
+				const resolvedDirectoryUploads = await Promise.all(directories)
+				currentUpload.status = UploadStatus.FINISHED
+				resolve([resolvedUploads, ...resolvedDirectoryUploads].flat())
+			} catch (e) {
+				abort.abort(e)
+				currentUpload.status = UploadStatus.FAILED
+				reject(e)
+			} finally {
+				if (directory.name) {
+					this._notifyAll(currentUpload)
+					this.updateStats()
+				}
+			}
+		})
+	}
+
+	/**
 	 * Upload a file to the given path
-	 * @param {string} destinationPath the destination path relative to the root folder. e.g. /foo/bar.txt
-	 * @param {File} file the file to upload
+	 * @param {string} destination the destination path relative to the root folder. e.g. /foo/bar.txt
+	 * @param {File|FileSystemFileEntry} fileHandle the file to upload
 	 * @param {string} root the root folder to upload to
 	 */
-	upload(destinationPath: string, file: File, root?: string): PCancelable<Upload> {
-		const destinationFile = `${root || this.root}/${destinationPath.replace(/^\//, '')}`
+	upload(destination: string, fileHandle: File|FileSystemFileEntry, root?: string): PCancelable<Upload> {
+		root = root || this.root
+		const destinationPath = `${root.replace(/\/$/, '')}/${destination.replace(/^\//, '')}`
 
 		// Get the encoded source url to this object for requests purposes
-		const { origin } = new URL(destinationFile)
-		const encodedDestinationFile = origin + encodePath(destinationFile.slice(origin.length))
+		const { origin } = new URL(destinationPath)
+		const encodedDestinationFile = origin + encodePath(destinationPath.slice(origin.length))
 
-		logger.debug(`Uploading ${file.name} to ${encodedDestinationFile}`)
+		logger.debug(`Uploading ${fileHandle.name} to ${encodedDestinationFile}`)
 
-		// If manually disabled or if the file is too small
-		// TODO: support chunk uploading in public pages
-		const maxChunkSize = getMaxChunksSize(file.size)
-		const disabledChunkUpload = maxChunkSize === 0
-			|| file.size < maxChunkSize
-			|| this._isPublic
-
-		const upload = new Upload(destinationFile, !disabledChunkUpload, file.size, file)
-		this._uploadQueue.push(upload)
-		this.updateStats()
-
-		// eslint-disable-next-line no-async-promise-executor
 		const promise = new PCancelable(async (resolve, reject, onCancel): Promise<Upload> => {
+			// Handle file system entries by retrieving the file handle
+			if (isFileSystemFileEntry(fileHandle)) {
+				fileHandle = await new Promise((resolve) => (fileHandle as FileSystemFileEntry).file(resolve, reject))
+			}
+			// We can cast here as we handled system entries in the if above
+			const file = fileHandle as File
+
+			// If manually disabled or if the file is too small
+			// TODO: support chunk uploading in public pages
+			const maxChunkSize = getMaxChunksSize('size' in file ? file.size : undefined)
+			const disabledChunkUpload = this._isPublic
+				|| maxChunkSize === 0
+				|| ('size' in file && file.size < maxChunkSize)
+
+			const upload = new Upload(destinationPath, !disabledChunkUpload, file.size, file)
+			this._uploadQueue.push(upload)
+			this.updateStats()
+
 			// Register cancellation caller
 			onCancel(upload.cancel)
 
@@ -271,7 +444,7 @@ export class Uploader {
 						reject('Failed assembling the chunks together')
 					} else {
 						upload.status = UploadStatus.FAILED
-						reject('Upload has been cancelled')
+						reject(t('Upload has been cancelled'))
 					}
 
 					// Cleaning up temp directory
@@ -282,11 +455,7 @@ export class Uploader {
 				}
 
 				// Notify listeners of the upload completion
-				this._notifiers.forEach(notifier => {
-					try {
-						notifier(upload)
-					} catch (error) {}
-				})
+				this._notifyAll(upload)
 			} else {
 				logger.debug('Initializing regular upload', { file, upload })
 
@@ -319,7 +488,7 @@ export class Uploader {
 					} catch (error) {
 						if (error instanceof CanceledError) {
 							upload.status = UploadStatus.FAILED
-							reject('Upload has been cancelled')
+							reject(t('Upload has been cancelled'))
 							return
 						}
 
@@ -334,19 +503,11 @@ export class Uploader {
 					}
 
 					// Notify listeners of the upload completion
-					this._notifiers.forEach(notifier => {
-						try {
-							notifier(upload)
-						} catch (error) {}
-					})
+					this._notifyAll(upload)
 				}
 				this._jobQueue.add(request)
 				this.updateStats()
 			}
-
-			// Reset when upload queue is done
-			this._jobQueue.onIdle()
-				.then(() => this.reset())
 			return upload
 		}) as PCancelable<Upload>
 
