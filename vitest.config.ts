0a0c7c6a880606985b9e86603f3d6c2d8ba5fed6
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-26 16:43:05 +0200

    fix: Refactor logger code and add l10n as ignored for code coverage
    It makes no sense to test the l10n part...
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,19 @@
-import config from './vite.config'
+import type { UserConfig } from 'vitest'
+import config from './vite.config.ts'
 
 export default async (env) => {
 	const cfg = await config(env)
 	// Node externals does not work with vitest
-	cfg.plugins = cfg.plugins!.filter((plugin) => plugin && plugin.name !== 'node-externals')
+	cfg.plugins = cfg.plugins!.filter((plugin) => plugin && 'name' in plugin && plugin.name !== 'node-externals')
 
 	cfg.test = {
 		environment: 'jsdom',
 		coverage: {
 			include: ['lib/**'],
-			exclude: ['lib/utils/logger.ts'],
-			provider: 'istanbul',
+			// This makes no sense to test
+			exclude: ['lib/utils/l10n.ts'],
 			reporter: ['lcov', 'text'],
 		},
-	}
+	} as UserConfig
 	return cfg
 }
