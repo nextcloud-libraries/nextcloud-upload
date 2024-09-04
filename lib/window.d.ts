ae5537dbbe0be4ae9171601e0fc7fde99d7603f4
Ferdinand Thiessen <opensource@fthiessen.de>
2024-09-04 16:57:14 +0200

    fix: Make the default uploader global
    This way it is possible e.g. for `files_sharing` and `files` to share the same uploader instance.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/window.d.ts
+++ lib/window.d.ts
@@ -3,10 +3,13 @@
  * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-export {}
+import type { Uploader } from './lib/uploader'
 
+// This is for private use only
 declare global {
 	interface Window {
+		_nc_uploader?: Uploader
+
 		OC: Nextcloud.v28.OC & {
 			appConfig: {
 				files: {
