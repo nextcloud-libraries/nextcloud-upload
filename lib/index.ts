3a86f7deaca7a20fee6489ddece7aa24aa0a1c95
Grigorii K. Shartsev <me@shgk.me>
2025-06-23 13:07:20 +0200

    refactor: add `ui` reexport
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- lib/index.ts
+++ lib/index.ts
@@ -3,12 +3,11 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-export { default as UploadPicker } from './ui/components/UploadPicker.vue'
+export type { ConflictResolutionResult, ConflictPickerOptions } from './ui/ui.ts'
+export { openConflictPicker, uploadConflictHandler, UploadPicker } from './ui/ui.ts'
+
 export type { IDirectory, Directory } from './core/utils/fileTree.ts'
 export { getConflicts, hasConflict } from './core/utils/conflicts.ts'
-export { uploadConflictHandler } from './ui/utils/uploadConflictHandler.ts'
 export { Upload, Status as UploadStatus } from './core/upload.ts'
 export * from './core/uploader'
-export { openConflictPicker } from './ui/openConflictPicker.ts'
-export type { ConflictResolutionResult, ConflictPickerOptions } from './ui/openConflictPicker.ts'
 export { getUploader, upload } from './core/getUploader.ts'
---
+++ lib/ui/ui.ts
@@ -0,0 +1,9 @@
+/*!
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+export type { ConflictResolutionResult, ConflictPickerOptions } from './openConflictPicker.ts'
+export { default as UploadPicker } from './components/UploadPicker.vue'
+export { openConflictPicker } from './openConflictPicker.ts'
+export { uploadConflictHandler } from './utils/uploadConflictHandler.ts'
