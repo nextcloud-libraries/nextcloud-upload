62ead94124bc5c2f316e4b53ae4a8b78b723f70a
Grigorii K. Shartsev <me@shgk.me>
2025-06-27 14:08:55 +0200

    refactor: reorder exports in index
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- lib/index.ts
+++ lib/index.ts
@@ -3,11 +3,17 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-export type { IDirectory, Directory } from './utils/fileTree'
-export { getConflicts, hasConflict } from './utils/conflicts.ts'
-export { uploadConflictHandler } from './dialogs/utils/uploadConflictHandler.ts'
-export { Upload, Status as UploadStatus } from './upload'
-export * from './uploader/index.ts'
-export { openConflictPicker } from './dialogs/openConflictPicker.ts'
-export type { ConflictResolutionResult, ConflictPickerOptions } from './dialogs/openConflictPicker.ts'
+export type { Eta, EtaEventsMap } from './uploader/index.ts'
+export type { IDirectory, Directory } from './utils/fileTree.ts'
+
 export { getUploader, upload } from './getUploader.ts'
+export { Upload, Status as UploadStatus } from './upload.ts'
+export { Uploader, UploaderStatus, EtaStatus, } from './uploader/index.ts'
+export { getConflicts, hasConflict } from './utils/conflicts.ts'
+
+export type { ConflictResolutionResult, ConflictPickerOptions } from './dialogs/openConflictPicker.ts'
+
+export { openConflictPicker } from './dialogs/openConflictPicker.ts'
+export { uploadConflictHandler } from './dialogs/utils/uploadConflictHandler.ts'
+
+export { default as UploadPicker } from './vue/components/UploadPicker.vue'
